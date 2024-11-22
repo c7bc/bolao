0269a9bb21e345176bce713e2db6b95304b80b6b
Daniel <dnnxzz70@gmail.com>
2024-11-22 00:45:21 -0300

    Changes to be committed: 	modified:   src/app/api/cliente/register/route.js

--- src/app/api/cliente/register/route.js
+++ src/app/api/cliente/register/route.js
@@ -5,10 +5,10 @@ import bcrypt from 'bcryptjs';
 import { v4 as uuidv4 } from 'uuid';
 
 const dynamoDbClient = new DynamoDBClient({
-  region: process.env.AWS_DEFAULT_REGION || 'sa-east-1',
+  region: process.env.REGION || 'sa-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 });
 
