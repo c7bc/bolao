5152dfc2ad9f7c82429b81cccdf9457ad8ae8b44
Daniel <dnnxzz70@gmail.com>
2025-01-19 16:59:16 -0300

    modified:   frontend/next.config.mjs 	modified:   frontend/package-lock.json

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      if (!dev) {
+        // Configuração para produção
+        config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
+          if (plugin.constructor.name === 'TerserPlugin') {
+            return new webpack.optimize.TerserPlugin({
+              ...plugin.options,
+              terserOptions: {
+                ...plugin.options.terserOptions,
+                cache: true,
+                parallel: true,
+                sourceMap: false,
+                memoryLimit: 512, // Limita a quantidade de memória utilizada para minificação
+                ecma: 2020,
+                compress: {
+                  ecma: 2020,
+                  passes: 2,
+                },
+                mangle: {
+                  safari10: true,
+                },
+              },
+            });
+          }
+          return plugin;
+        });
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
