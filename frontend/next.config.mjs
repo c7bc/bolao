c19ac9133185755ea460cc6676a28fc9a4ee1a55
Daniel <dnnxzz70@gmail.com>
2025-01-19 13:28:00 -0300

    Changes to be committed: 	deleted:    amplify.yml 	modified:   frontend/next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, // Ignora erros de ESLint durante o build
-    },
-    typescript: {
-      ignoreBuildErrors: true, // Ignora erros de tipagem durante o build
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
